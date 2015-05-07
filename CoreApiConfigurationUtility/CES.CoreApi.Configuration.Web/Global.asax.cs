4063cd21ff74732149b101ff9a008d952a4d9742
Oleg Kuznetsov <okuznetsov@riafinancial.com>
2015-05-07 14:49:59 -0700

    Exception handling added to global

--- CoreApiConfigurationUtility/CES.CoreApi.Configuration.Web/Global.asax.cs
+++ CoreApiConfigurationUtility/CES.CoreApi.Configuration.Web/Global.asax.cs
@@ -4,6 +4,7 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using CES.CoreApi.Logging.Interfaces;
 
 namespace CES.CoreApi.Configuration.Web
 {
@@ -18,14 +19,16 @@ namespace CES.CoreApi.Configuration.Web
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
             MapperConfig.Configure();
-            throw new Exception("Test");
         }
-        
-        //protected void Application_Error(object sender, EventArgs e)
-        //{
-        //    Exception exception = Server.GetLastError();
-        //    Server.ClearError();
-        //    Response.Redirect("/Home/Error");
-        //}
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            var monitorFactory = DependencyResolver.Current.GetService<ILogMonitorFactory>();
+            if (monitorFactory == null) //if exception happened during dependency registration
+                return;
+            var exceptionMonitor = monitorFactory.CreateNew<IExceptionLogMonitor>();
+            var exception = Server.GetLastError();
+            exceptionMonitor.Publish(exception);
+        }
     }
 }
