227f231416e9f1b14e3c3ceebe6d23db3a2efae8
Oleg Kuznetsov <okuznetsov@riafinancial.com>
2015-07-15 10:16:47 -0700

    Old files removed

---
+++ bak/BeneficiaryNameAndCountryValidationModel.cs
@@ -0,0 +1,9 @@
+namespace CES.CoreApi.OrderValidation.Service.Business.Contract.Models
+{
+    public class BeneficiaryNameAndCountryValidationModel
+    {
+        public string FirstName { get; set; }
+        public string LastName1 { get; set; }
+        public string Country { get; set; }
+    }
+}
