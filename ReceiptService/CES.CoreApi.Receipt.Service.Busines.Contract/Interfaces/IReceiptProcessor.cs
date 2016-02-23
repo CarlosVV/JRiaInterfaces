c764e6dfa86e950d46c7332becd91a8f94305180
Yib <Yibeltalh@dntg.net>
2016-02-23 11:48:34 -0800

    removing unused projects

--- ReceiptService/CES.CoreApi.Receipt.Service.Busines.Contract/Interfaces/IReceiptProcessor.cs
+++
@@ -1,9 +0,0 @@
-﻿using CES.CoreApi.Receipt.Service.Business.Contract.Models;
-
-namespace CES.CoreApi.Receipt.Service.Business.Contract.Interfaces
-{
-    public interface IReceiptProcessor
-    {
-        ReceiptModel GenerateReceipt(int id);
-    }
-}
--- ReceiptService/CES.CoreApi.Receipt.Service.Busines.Contract/Models/ReceiptModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CES.CoreApi.Receipt.Service.Business.Contract.Models
-{
-    public class ReceiptModel
-    {
-        public byte[] ReceiptBody { get; set; }
-        public string ReceiptName { get; set; }
-    }
-}
