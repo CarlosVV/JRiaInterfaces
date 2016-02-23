0c633e7275cc4d02b728c73c14dd6a5310d883db
Yib <Yibeltalh@dntg.net>
2016-02-22 19:15:38 -0800

    removing this project

--- AccountingService/CES.CoreApi.Accounting.Service.Business.Contract/Interfaces/ITransactionInformationProcessor.cs
+++
@@ -1,9 +0,0 @@
-﻿using CES.CoreApi.Accounting.Service.Business.Contract.Models;
-
-namespace CES.CoreApi.Accounting.Service.Business.Contract.Interfaces
-{
-    public interface ITransactionInformationProcessor
-    {
-        TransactionSummaryModel GetTransactionSummary(GetTransactionSummaryRequestModel request);
-    }
-}
--- AccountingService/CES.CoreApi.Accounting.Service.Contract/Constants/Namespaces.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CES.CoreApi.Accounting.Service.Contract.Constants
-{
-    public static class Namespaces
-    {
-        public const string AccountingServiceContractNamespace = "http://riafinancial.com/services/composite/Accounting/v1";
-        public const string AccountingServiceDataContractNamespace = "http://riafinancial.com/services/composite/Accounting/Data/v1";
-    }
-}
