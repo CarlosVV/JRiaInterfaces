ededf073b24a5f87d1e386dcc45f54095a778df9
Yib <Yibeltalh@dntg.net>
2016-04-06 18:57:42 -0700

    adding app insights package

---
+++ Foundation/CES.CoreApi.Foundation/Configuration/GeoLocationConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+
+namespace CES.CoreApi.Foundation.Configuration
+{
+	public class GeoLocationConfiguration : ConfigurationElement
+	{
+		[ConfigurationProperty("url")]
+		public string Url => (string)this["url"];
+
+		[ConfigurationProperty("key")]
+		public string Key => (string)this["key"];
+
+	}
+}
---
+++ Foundation/CES.CoreApi.Security/Interfaces/IApplicationAuthenticator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.IdentityModel.Policy;
+using System.ServiceModel.Channels;
+
+namespace CES.CoreApi.Foundation.Security.Interfaces
+{
+    public interface IApplicationAuthenticator
+    {
+        ReadOnlyCollection<IAuthorizationPolicy> Authenticate(ReadOnlyCollection<IAuthorizationPolicy> authPolicy, 
+            Uri listenUri, ref Message message);
+    }
+}
