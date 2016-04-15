42b9429397bda1b35c40f05da1af62e6b462b9e4
Javier De La Blanca <jblanca@riafinancial.com>
2016-04-15 18:31:17 +0200

    Added missing File.
    Related Work Items: #57783

---
+++ Foundation/CES.CoreApi.Security/Interfaces/IWCFRequestHeaderParametersProvider.cs
@@ -0,0 +1,10 @@
+﻿using CES.CoreApi.Common.Models;
+using CES.CoreApi.Foundation.Models;
+
+namespace CES.CoreApi.Security.Interfaces
+{
+    public interface IWCFRequestHeaderParametersProvider
+	{
+		ServiceCallHeaderParameters GetParameters();
+	}
+}
