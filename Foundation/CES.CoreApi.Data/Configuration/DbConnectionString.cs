866af5353081bc1c848c41e92a7043e8f80f8ffe
Javier De La Blanca <jblanca@riafinancial.com>
2016-04-26 16:10:17 +0200

    Missing files

--- Foundation/CES.CoreApi.Data/Configuration/DbConnectionString.cs
+++ Foundation/CES.CoreApi.Data/Configuration/DbConnectionString.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Configuration;
 
-namespace CES.CoreApi.Foundation.Data.Configuration
+namespace CES.CoreApi.Data.Configuration
 {
 	public class DbConnectionString
 	{
