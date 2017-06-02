2136d82c75d564355d7f07a3a3981e4807cc0f6d
cvalderrama <cvalderrama@riafinancial.com>
2017-06-02 06:23:37 -0700

    AddEditRanges in Model Window for Download Documents

---
+++ CES.CoreApi.Receipt_Main/CES.CoreApi.Receipt_Main.UI.WPF/ViewModel/AddEditRangesViewModel.cs
@@ -0,0 +1,18 @@
+﻿using CES.CoreApi.Receipt_Main.Domain.Core.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CES.CoreApi.Receipt_Main.UI.WPF.ViewModel
+{
+    public class AddEditRangesViewModel : ViewModelBase
+    {
+        public IDocumentService documentService;
+        public AddEditRangesViewModel(IDocumentService documentService)
+        {
+            this.documentService = documentService;
+        }
+    }
+}
